4d6dd13d6c2ef02db17b51dc7b848426cf2ea618
Laurent Lam <laurent.lam@ensta-paristech.fr>
2018-04-17 15:04:26 +0200

    Adaptation 2D: Classe Environment fonction step

--- Code_base/environments/Environment.py
+++ Code_base/environments/Environment.py
@@ -1,7 +1,15 @@
 __author__ = "Florence Carton"
 __credits__ = ["Florence Carton", "Freek Stulp", "Antonin Raffin"]
 
+import random
+import time
+
 class Environment:
+
+	LEFT = 0
+	RIGHT = 1
+	UP = 2
+	DOWN = 3
 
 	def __init__(self):
 
@@ -9,22 +17,36 @@ class Environment:
 
 	def step(self, action):
 		"""Project environment one step into the future.
-        
+
          Given an action, compute the reward and next state, and whether next_state is terminal
-            
+
          Args:
              action : The action the agent is performing
-             
+
          Returns:
-			next_state : an observation of the next state 
+			next_state : an observation of the next state
 			reward : the reward the agent receives for performing action in the current state
 			is_done = if the next_state is a terminal state
         """
+		self.num_states = int(params['num_cells'])
 
+        # Check if there are enough cells
+        assert self.num_states>1, "Number of cells must be 2 or larger"
+
+        # Number of actions is 4 maximum (LEFT, RIGHT, UP, DOWN)
+        self.num_actions = 4
+
+        # Set to most right cell (will change in reset(...) anyway)
+        self.current_state  =  0
+
+        # end state
+        self.terminal_state = 0 # arbitrary
+
+        self.viewer = None
 		raise NotImplementedError('subclasses must override step()!')
 
 	def render(self):
-		""" Display the environment 
+		""" Display the environment
 		"""
 
 		raise NotImplementedError('subclasses must override render()!')
@@ -32,10 +54,10 @@ class Environment:
 
 	def reset(self):
 		"""Reset the environment.
-        
+
          Args:
-             
-             
+
+
          Returns:
 			first_state : the first state
         """
@@ -43,6 +65,6 @@ class Environment:
 
 
 	def close(self):
-		""" Close the environment 
+		""" Close the environment
 		"""
 		raise NotImplementedError('subclasses must override close()!')
