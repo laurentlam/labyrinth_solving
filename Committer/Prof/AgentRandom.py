77544588962f55aa5b5b398711bc651a0b12d94b
Luc ZORRILLA <luc.zorrilla@ensta-paristech.fr>
2018-04-27 22:43:18 +0200

    Added teacher's slides and coding

---
+++ Committer/Prof/AgentRandom.py
@@ -0,0 +1,19 @@
+import ...
+
+class AgentRandom:
+    """Votre description"""
+
+    def __init__(self,name):
+        """Description de l'initialisation"""
+
+    def startState(self, laby):
+        states=laby.initialStates();
+		...
+		return(states[0])
+
+	def startState(self, laby):
+        states=laby.initialStates();
+		...
+		# pos=random(0,size(states)-1)
+		
+		return(states[pos])
