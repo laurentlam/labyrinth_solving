9f34436f07f4da7fc4af9c6085bcb9411e6feb08
lam <laurent.lam@ensta-paristech.fr>
2018-04-28 16:05:14 +0200

    Updated RandomAgent.py: Updated description for RandomAgent and algorithm

--- Committer/Prof/AgentRandom.py
+++ Committer/Prof/AgentRandom.py
@@ -7,47 +7,29 @@ class AgentRandom:
         """Initializes a new agent.
         """
 
-        self.num_states = params['num_states']
-        self.num_actions = params['num_actions']
-
     def startState(self, laby):
-        """Start an episode with state 'initial_state', return first action
+        """Start an episode with initial labyrinthe, return position of start
         Args:
-            initial_state (int): The first states the agent makes
+            laby : The initial maze created
         Returns:
-            The first action the agent performs
+            The starting position of the agent
         """
 
         states=laby.initialStates();
-        action = random.randint(0,self.num_actions-1) # Returns a random action
 
 		return(states[0])
 
 	def nextAction(self, laby):
-        """One step of the agent in the reinforcement learning loop.
-
-        Usually, this function will do two things:
-        1) Use the reward to update values
-        2) Call the agent's policy to determine the next action, given
-           the state
+        """One step of the random agent
 
         Args:
-            reward (float): The reward recieved for performing the previous
-                action in the previous state
-            state : The state at the current time step
+            laby : The state at the current time step
         Returns:
             The action returned for the state at the current time step
         """
-        """The policy of an agent.
 
-        In reinforcement learning, the policy returns an action, given the
-        current state.
+        state = laby.currentState();
+        actions = laby.possibleActions();
+        action = actions[random.randint(0,len(actions)-1)]; # Returns a random action
 
-        Args:
-            state : The state the agent is in.
-        Returns:
-            The action (int) the agent performs.
-        """
-        actions=laby.possibleActions();
-		...
-		return(actions[0])
+		return(action)
