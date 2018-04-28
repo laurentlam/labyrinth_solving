6ba381a80c6f30c639b05eddd0701a3b416cd64e
lam <laurent.lam@ensta-paristech.fr>
2018-04-28 17:27:59 +0200

    Updated Labyrinthe.py: Added basic descriptions to few methods up to terminalStates

--- Committer/Prof/Labyrinthe.py
+++ Committer/Prof/Labyrinthe.py
@@ -1,10 +1,13 @@
-import ...
+import ENV from environment
 
 class labyrinthe:
     """Votre description"""
 
     def __init__(self,longueur,largeur):
-        """Description de l'initialisation"""
+        #Dimensions of the environment
+        self.width = largeur
+        self.length = longueur
+        self.states = environment.create_random_ENV(width,length)
 
     def show(self):
         """Description de la methode d'affichage choisie"""
@@ -13,51 +16,63 @@ class labyrinthe:
 
     def states(self):
         """Description de la methode"""
-		...
-		return([])
+		states = self.states
+		return(states)
 
-		
+
      def isInitialState(self, state):
-        """Description de la methode"""
-		...
-		return(false)
+        """Check whether state is a starting position, return a boolean"""
+		if (state.name!="s"):
+		    return(False)
+        else:
+            return(True)
 
     def initialStates(self):
-        """Description de la methode"""
-		...
-		return([])
+        """Return list of all possible starting positions"""
+        initStates = []
+        for i in range(length):
+            for j in range(width):
+                if isInitialState(self.states[i][j]):
+                   initStates+=self.states[i][j]
+		return(initStates)
 
-		
+
     def isTerminalState(self, state):
-        """Description de la methode"""
-		...
-		return(false)
+        """Check whether state is a finish position, return a boolean"""
+		if (state.name!="a"):
+		    return(False)
+        else:
+            return(True)
 
     def terminalStates(self):
-        """Description de la methode"""
-		...
-		return([])
+        """Return list of all possible starting positions"""
+        termStates = []
+        for i in range(length):
+            for j in range(width):
+                if isTerminalState(self.states[i][j]):
+                   termStates+=self.states[i][j]
+		return(termStates)
 
-		
+
 	def currentState(self):
         """Description de la methode"""
 		...
 		return(self.currentState)
 
-		
+
 	def possibleActions(self):
          """Description de la methode"""
 		...
 		return(['N', 'S', 'E', 'O'])
 
-	
+
 	def runStep(self, action):
          """Description de la methode"""
 		 rewward=0
 		...
-		return (self.currentState, rewward)
-		
-		
+		return (self.currentState, reward)
+
+
     def transition(self):
         """Description de la methode"""
 		...
@@ -73,6 +88,3 @@ class labyrinthe:
 		 rewward=0
 		...
 		return (self.currentState, rewward)
-		
-		
-		
