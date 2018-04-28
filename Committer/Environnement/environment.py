32435631f7631d7af47425d6c36a003bcd6b8c15
Luc ZORRILLA <luc.zorrilla@ensta-paristech.fr>
2018-04-28 02:30:52 +0200

    Still need to change numpy in environment

--- Committer/Environnement/environment.py
+++ Committer/Environnement/environment.py
@@ -1,5 +1,5 @@
-'''''' Environment and state classes ''''''
-#numpy is needed for matrix operations
+# '''''' Environment and state classes ''''''
+#numpy is needed for matrix operations ---> Need to use pure 2D-list manipulation
 import numpy
 #random is needed for random initialisations
 import random
@@ -11,22 +11,25 @@ import random
 class state:
     def __init__(self,name):
         self.name=name
-    #4 states exists : hole (can go), wall (can't go), start and arrival, as :
-    List_of_states=["o","x","s","a"]
+
+    def arrival_state(self):
+        self.name="a"
 
     #definition of random functions, according to restrictions of a maze.
 
     def random_intern_state(self):
+        #4 states exists : hole (can go), wall (can't go), start and arrival, as :
+        List_of_states=["o","x","s","a"]
         #picking a random state allowed within the maze
         dice=random.randint(1,2)
-        random_state.name=List_of_states[dice]
-        return random_state
+        self.name=List_of_states[dice]
 
     def random_extern_state(self):
+        #4 states exists : hole (can go), wall (can't go), start and arrival, as :
+        List_of_states=["o","x","s","a"]
         #picking a random state allowed on the border of the maze, but not an arrival as it is unique.
         dice=random.randint(1,3)
-        random_state.name=List_of_states[dice]
-        return random_state
+        self.name=List_of_states[dice]
 
 
 class ENV:
@@ -52,30 +55,30 @@ class ENV:
         self.length=length
 
         #List of states
-        states=numpy.zeros(width,length)
+        states=numpy.zeros((width,length))
         #creation of states within the maze
         for i in range (width-1):
-            for j in range(length-1):
-                states[i,j]=state.random_intern_state()
+            for j in range (length-1):
+                states[i,j]=state.random_intern_state(state)
 
         #creation of states around the maze
         for j in range(length):
-            states[-1,j]=state.random_extern_state()
-            states[0,j]=state.random_extern_state()
+            states[-1,j]=state.random_extern_state(state)
+            states[0,j]=state.random_extern_state(state)
         for i in range(1,-1): # specific borns to exclude already assigned states
-            states[i,-1]=state.random_extern_state()
-            states[i,0]=state.random_extern_state()
+            states[i,-1]=state.random_extern_state(state)
+            states[i,0]=state.random_extern_state(state)
 
         #creation of the arrival (substitution to an external state)
         bool_border=-random.randint(0,1) #to get 0 or -1
         [i,j]=[bool_border,bool_border]
         I_OR_J=random.randint(0,1)
-        [i,j][1-I_OR_J]=random.randint(size(states)[1-I_OR_J]) #randomizing again on whole dimension the unchosen border
-        states[i,j]="a" #arrival can now be put there
+        [i,j][1-I_OR_J]=random.randint(1,states.shape[1-I_OR_J]) #randomizing again on whole dimension the unchosen border
+        states[i,j]=states[i,j].arrival_state(state) #arrival can now be put there
 
         #States of the environment are now fully randomly created
         self.states=states
 
-random_environment=ENV()
+random_environment=ENV(None,None,[None])
 random_environment.create_random_ENV()
 random_environment.Visualise()
