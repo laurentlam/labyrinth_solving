78401e528a98c867fd0d3c4ba75d5e6d02fb3498
Luc ZORRILLA <luc.zorrilla@ensta-paristech.fr>
2018-04-28 17:41:52 +0200

    Added comments in Labyrinthe.py

--- Committer/Prof/Labyrinthe.py
+++ Committer/Prof/Labyrinthe.py
@@ -5,59 +5,69 @@ class labyrinthe:
 
     def __init__(self,longueur,largeur):
         """Description de l'initialisation"""
+        #DONE --> Added "states" attribut
 
     def show(self):
         """Description de la methode d'affichage choisie"""
+        #DONE
 		...
 		print("Mon labyrinthe de maniere ostensible mais simple")
 
     def states(self):
         """Description de la methode"""
+        #NOT NEEDED --> states is now an attribut
 		...
 		return([])
 
-		
+
      def isInitialState(self, state):
         """Description de la methode"""
-		...
+        #TO DO --> easy : find the "s" states, meaning with a name_index of 2
+        ...
 		return(false)
 
     def initialStates(self):
         """Description de la methode"""
+        #TO DO --> easy : lists positions of "s" states (can use isInitialState)
 		...
 		return([])
 
-		
+
     def isTerminalState(self, state):
         """Description de la methode"""
+        #TO DO --> easy : find the "a" (unique) state, meaning with a name_index of 3
 		...
 		return(false)
 
     def terminalStates(self):
         """Description de la methode"""
+        #TO DO --> easy : lists positions of "a" states (can use isTerminalState)
+        #Note : arrival state is unique in this modelization
 		...
 		return([])
 
-		
+
 	def currentState(self):
         """Description de la methode"""
+        #TO DO --> need to have the position (global variable ?) of the current state
 		...
 		return(self.currentState)
 
-		
+
 	def possibleActions(self):
          """Description de la methode"""
+         #TO DO --> Need to find the currentState first
 		...
 		return(['N', 'S', 'E', 'O'])
 
-	
+
 	def runStep(self, action):
          """Description de la methode"""
-		 rewward=0
+		 reward=0
 		...
 		return (self.currentState, rewward)
-		
-		
+
+
     def transition(self):
         """Description de la methode"""
 		...
@@ -73,6 +83,3 @@ class labyrinthe:
 		 rewward=0
 		...
 		return (self.currentState, rewward)
-		
-		
-		
