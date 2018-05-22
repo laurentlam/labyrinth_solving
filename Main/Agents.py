61ad78515adca53dd2bff3a6a94868d61024dd75
Luc ZORRILLA <luc.zorrilla@ensta-paristech.fr>
2018-05-22 18:02:57 +0200

    Created Agents.py to do an Agent general CLASS

---
+++ Main/Agents.py
@@ -0,0 +1,20 @@
+class Agent:
+
+    def __init__(self,arguments):
+        """ General class for agent : can contain parameters  """
+        self.parameters=arguments
+
+
+
+    class nextAction:
+
+        """ To get nextAction, do Agent.nextAction.nextAction()"""
+
+        def __init__(self):
+
+        def startState(self,laby):
+            return laby.current_position
+
+        def nextAction(self):
+
+            return(action)
